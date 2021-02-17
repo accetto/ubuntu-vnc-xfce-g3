bfd5344c56065b254b5b43b1894eb1c6cb38dcff
accetto <34798830+accetto@users.noreply.github.com>
2021-02-17 19:36:38 +0100

    minor changes in hook scripts

--- docker/hooks/release_of.sh
+++ docker/hooks/release_of.sh
@@ -5,11 +5,16 @@ main() {
 
     case "$1" in
 
-        chromium | chromium-browser | chromiumbrowser)
+        chromium-1804 )
             result=$( \
                 curl -s http://archive.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ \
                 | grep -Po -m1 '(?<=href=")[^_]*_([0-9.]+-0ubuntu0\.18\.04\.[^_"]*)_[^"]*' \
                 | cut -d _ -f 2 )
+            ;;
+
+        * )
+            echo "Unknown key '$1'"
+            return 1
             ;;
 
     esac
