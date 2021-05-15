481fa43ca3d033aa7d83f28967c065a0c5656578
accetto <34798830+accetto@users.noreply.github.com>
2021-05-15 19:39:51 +0200

    script 'releas_of.sh' improved

--- docker/hooks/release_of.sh
+++ docker/hooks/release_of.sh
@@ -6,10 +6,12 @@ main() {
     case "$1" in
 
         chromium-1804 )
-            result=$( \
-                curl -s http://archive.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ \
+            # result=$(curl -sL \
+            result=$(wget -qO- \
+                http://archive.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ \
                 | grep -Po -m1 '(?<=href=")[^_]*_([0-9.]+-0ubuntu0\.18\.04\.[^_"]*)_[^"]*' \
-                | cut -d _ -f 2 )
+                | cut -d _ -f 2 \
+            )
             ;;
 
         * )
